485fd82bd3835ff04ac587c4b36a10e0e0aff714
atulsomark <achaudhary@somarkinnovations.com>
2018-03-01 22:40:34 +1030

    working with operator in java

---
+++ src/com/semanticsquare/thrillio/Operator.java
@@ -0,0 +1,30 @@
+package com.semanticsquare.thrillio;
+
+public class Operator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		coumpoundOperator();
+	}
+
+	private static void coumpoundOperator() {
+		int x = 10;
+		System.out.println("Before compund operator:- " + x);
+		x -= 5;
+		System.out.println("Before compund operator:- " + x);
+		x = +x;
+		System.out.println("When used x =+ x, value of x is " + x);
+		x = -x;
+		System.out.println("When used x =- x, value of x is " + x);
+
+		int value = 'a' + 'b';
+		System.out.println("'a'+'b'=" + value);
+
+		double d = 1 / 2;
+		System.out.println("1/2 when assigned to double variable = " + d);
+
+		double dMod = 1 % 2;
+		System.out.println("1%2 when assigned to double variable = " + dMod);
+	}
+
+}
